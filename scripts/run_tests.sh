0854e4d17d1c97e5b82967b1dc825617310c2c8d
Kamila Souckova <kamila@vesmir.sk>
2012-09-03 08:19:54 +0200

    make the JASMINE command include options

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,24 +6,26 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 TEST_DIR="$REPO_ROOT/test"
 RUN_APP='cd "$REPO_ROOT"; npm start'
 
-JASMINE="$REPO_ROOT/node_modules/.bin/jasmine-node"
+JASMINE="$REPO_ROOT/node_modules/.bin/jasmine-node --color --coffee"
 
 
 HL='\033[0;37m'; RESET='\033[0m'
 
 echo -e "$HL------------ unit ------------$RESET"
-"$JASMINE" --color --coffee "$TEST_DIR/unit"
+$JASMINE "$TEST_DIR/unit"
 STATUS=$?
 
 [[ $STATUS != 0 ]] && exit $STATUS
 
 echo -e "$HL------------ app -------------$RESET"
 
-sh -c "$RUN_APP &> '$TEST_DIR/app/last_run.log'" &
+export PORT=4999
+
+sh -c "export PORT=4999; $RUN_APP &> '$TEST_DIR/app/last_run.log'" &
 SERVER_PID=$!
 sleep 1 # start-up
 
-"$JASMINE" --color --coffee "$TEST_DIR/app"
+$JASMINE "$TEST_DIR/app"
 STATUS=$?
 
 kill $SERVER_PID
